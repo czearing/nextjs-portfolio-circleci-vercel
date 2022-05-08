044225043bcb095b3133650f3919718178488924
czearing <czearing@outlook.com>
2022-05-08 16:17:50 -0700

    Cleaning up package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
    <div>
-     Hello world this is my portfolio
+     Hello world this is my portfolio.
    </div>
   )
 }
